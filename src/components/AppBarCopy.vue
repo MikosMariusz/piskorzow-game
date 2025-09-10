e73e3a416779c01657cc6a68357d803001eea31a
Mariusz <mmicustommaps@gmail.com>
2025-09-10 19:51:37 +0200

    chore: appbar

--- src/components/AppBarCopy.vue
+++ src/components/AppBarCopy.vue
@@ -1,59 +1,52 @@
 <template>
-    <v-app-bar
-        :image="bg"
-        style="z-index: 1300"
-    >
+    <v-app-bar :image="bg">
         <template v-slot:image>
             <div class="gradient-container">
                 <v-img
                     :src="bg"
                     :gradient="darkGradient"
                     class="gradient-layer"
-                    :style="{ opacity, transitionDuration: `${getOSMDuration()}ms` }"
+                    :style="{ opacity, transitionDuration: `${700}ms` }"
                 ></v-img>
                 <v-img
                     :src="bg"
                     :gradient="osmGradient"
                     class="gradient-layer"
-                    :style="{ opacity: 1 - opacity, transitionDuration: `${getOSMDuration()}ms` }"
+                    :style="{ opacity: 1 - opacity, transitionDuration: `${700}ms` }"
                 ></v-img>
             </div>
         </template>
-        <v-toolbar-title :class="{ 'text-shadow text-white': appStore.isDarkEnabled }">{{
-            $t('gameTitle')
-        }}</v-toolbar-title>
-        <v-spacer />
+        <v-toolbar-title>{{ $t('gameTitle') }}</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
         <v-btn
-            icon="mdi-information-outline"
-            variant="elevated"
-            size="small"
-            elevation="4"
-            rounded="sm"
-            class="mr-5"
+            @click="goToGame"
+            variant="outlined"
             color="primary"
-            :title="$t('projectInfo.showInfo')"
-            @click="$emit('show-project-info')"
-            @contextmenu.prevent="$emit('reset-project-info')"
-        />
-        <lang-switcher class="mr-4" />
+        >
+            Game
+        </v-btn>
     </v-app-bar>
 </template>
 
 <script setup>
-import { computed } from 'vue'
 import { useAppStore } from '@/stores/app'
-import { getOSMDuration } from '@/services/olMap'
-
-defineEmits(['show-project-info', 'reset-project-info'])
+import { useRouter } from 'vue-router'
 
 const bg = new URL('../assets/images/bar-background.png', import.meta.url).href
 const appStore = useAppStore()
+const router = useRouter()
 
 const darkGradient = 'to top right, rgba(34,34,34,0.8), rgba(60,60,60,0.7)'
 const osmGradient =
     '90deg, rgba(182,227,182,0.7) 0%, rgba(120,200,120,0.7) 50%, rgba(163,193,218,0.5) 100%'
 
 const opacity = computed(() => (appStore.isDarkEnabled ? 1 : 0))
+
+const goToGame = () => {
+    router.push('/game')
+}
 </script>
 
 <style scoped>
